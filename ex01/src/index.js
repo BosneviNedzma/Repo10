a1e56c162f3f8e382fe3a8b189b1f095db2609bb
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-22 12:02:19 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+   var myPet = {
+       species: "Maltezer",
+       name: "Mehinica",
+       legs : 4,
+       friends: "Meho, Huso"
+   } 
+   function myFunction (myObj){ 
+       return myObj;
+   }
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction};
